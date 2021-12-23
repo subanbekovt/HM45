184038ca793c1fdeccbc6f1e5d685325ea64e20c
tugolbay-subanbekov <subanbekovt@gmail.com>
2021-12-23 14:14:50 +0600

    Приложение прописано в setting.py, добавлена модель и выполнена миграция

--- HM45/settings.py
+++ HM45/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom apps
+    "To_Do_list"
 ]
 
 MIDDLEWARE = [
--- To_Do_list/models.py
+++ To_Do_list/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class ToDoList(models.Model):
+    description = models.TextField(max_length=1000, null=False, blank=False, verbose_name="Описание")
+    status = models.CharField(max_length=200, null=False, blank=False, verbose_name="Статус", default="new")
+    due_date = models.DateField(null=True, blank=True, verbose_name="Дата выполнения")
+
+    def __str__(self):
+        return f"{self.pk}. {self.description} - {self.status}"
+
+    class Meta:
+        db_table = 'to_do_lists'
+        verbose_name = "Список дел"
+        verbose_name_plural = "Списки дел"
