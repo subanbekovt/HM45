bfe8b83d8286da429f3abb9f73f38ded8499efc4
tugolbay-subanbekov <subanbekovt@gmail.com>
2021-12-23 18:56:26 +0600

    Добавлены view-вы

--- To_Do_list/urls.py
+++ To_Do_list/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from To_Do_list.views import index_view
+from To_Do_list.views import index_view, add_view, new_entry
 
 urlpatterns = [
     path('', index_view),
+    path('add/', add_view),
+    path('add/new/', add_view)
 ]
--- To_Do_list/views.py
+++ To_Do_list/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 
+from To_Do_list.models import ToDoList
+from To_Do_list.models import STATUS_CHOICES
+
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'index.html')
+    todo = ToDoList.objects.order_by("status")
+    return render(request, 'index.html', {'todo': todo})
+
+
+def add_view(request):
+    if request.method == 'GET':
+        return render(request, 'new.html', {'status': STATUS_CHOICES})
+    elif request.method == 'POST':
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        due_date = request.POST.get('status')
+        if due_date == '':
+            due_date = None
+        entry = ToDoList.objects.create(description=description, status=status, due_date=due_date)
+        # context = {
+        #     "entry": entry
+        # }
+        return render(request, 'new.html')
+
+
+def new_entry(request):
+    return render(request, "one_entry.html")
+
+
+def task_view(request):
+    todo = ToDoList.objects.order_by("status")
+    return render(request, 'one_entry.html', {{'todo': todo}})
