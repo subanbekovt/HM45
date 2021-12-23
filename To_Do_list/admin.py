b2213c14eb408f067a3c838be6ad451059b803a7
tugolbay-subanbekov <subanbekovt@gmail.com>
2021-12-23 16:15:35 +0600

    Добавлен селект status в модель, добавлен класс ToDoAdmin

--- To_Do_list/admin.py
+++ To_Do_list/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from To_Do_list.models import ToDoList
 
-admin.site.register(ToDoList)
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'due_date']
+    list_filter = ['status']
+    search_fields = ['description', 'status']
+    fields = ['description', 'status', 'due_date']
+    readonly_fields = ['due_date']
+
+
+admin.site.register(ToDoList, TodoAdmin)
--- To_Do_list/models.py
+++ To_Do_list/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 # Create your models here.
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
 
 
 class ToDoList(models.Model):
     description = models.TextField(max_length=1000, null=False, blank=False, verbose_name="Описание")
-    status = models.CharField(max_length=200, null=False, blank=False, verbose_name="Статус", default="new")
+    status = models.CharField(max_length=200, null=False, blank=False, verbose_name="Статус", default="new", choices=STATUS_CHOICES)
     due_date = models.DateField(null=True, blank=True, verbose_name="Дата выполнения")
 
     def __str__(self):
